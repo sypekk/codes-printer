4fa32791127577c6aa8050a23845dd2ab155503a
Michał Sypniewski <mi.sypniewski@gmail.com>
2019-04-02 12:53:20 +0200

    Updates

---
+++ precache-manifest.082942dff055e5caa731066f1ad75868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a424218ed465dbf6f28a",
+    "url": "/codes-printer/static/js/runtime~main.a424218e.js"
+  },
+  {
+    "revision": "7b70785897412c5e7588",
+    "url": "/codes-printer/static/js/main.7b707858.chunk.js"
+  },
+  {
+    "revision": "d38aa31bbb5e37f0860a",
+    "url": "/codes-printer/static/js/1.d38aa31b.chunk.js"
+  },
+  {
+    "revision": "7b70785897412c5e7588",
+    "url": "/codes-printer/static/css/main.c5881e9f.chunk.css"
+  },
+  {
+    "revision": "54f5d9d2a57033a9348e96418335e44a",
+    "url": "/codes-printer/index.html"
+  }
+];

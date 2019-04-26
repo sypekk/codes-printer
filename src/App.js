7b6e0d0eb79b10d558cf28b4dd43dc2e0fceda71
Michał Sypniewski <mi.sypniewski@gmail.com>
2019-04-26 14:19:36 +0200

    change flex to grid in css,

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import Inputer from './Inputer';
 
 
 const Item = (props) => (
-  <p>{props.index % 2 ? <span>{props.title}</span> : null}<span className="code">{`*${props.item.toUpperCase()}*`}</span></p>
+  // <p>{props.index % 2 ? <span>{props.title}</span> : null}<span className="code">{`*${props.item.toUpperCase()}*`}</span></p>
+
+  <div className='code'>
+    {`*${props.item.toUpperCase()}*`}
+    {/* <span>{props.title}</span> */}
+    {/* <span className="code">{`*${props.item.toUpperCase()}*`}</span> */}
+  </div>
 )
 
+
+const Title = (props) => (
+  <p>
+    <span>{props.title}</span>
+  </p>
+)
 
 class App extends Component {
 
   counter = 0;
-  index = 0;
+  indx = 0;
 
   state = ({
     text: '',
@@ -19,26 +31,47 @@ class App extends Component {
     off: false,
     title: '',
     titleTable: [],
+    // colums: 2,
   })
+
+  addItem = (e, newItem) => {
+    e.preventDefault();
+
+    this.setState(prevState => ({
+      [newItem.type]: [...prevState[newItem.type], newItem]
+    }))
+  }
 
   handleButtonAdd = () => {
     const lista = this.state.list;
     const text = this.state.text;
     const title = this.state.title;
     const titleTable = this.state.titleTable;
-    const index = this.index;
+    const indx = this.indx;
+    // const colums = this.state.colums;
     const split = text.split(' ');
-    split.forEach(item => {
+    console.log(split)
+    split.forEach((item, index) => {
+      lista.push({
+        item: title,
+      })
       const tmp = item.split('\t');
-      tmp.forEach(item => {
+      console.log(tmp);
+      //to jest kluczem do sukcesu
+      tmp.forEach((item, index2) => {
+        console.log('pojedyncze cos ' + item);
+        // if (index % { colums }) {
         lista.push({
-          index,
           item,
         });
+        // }
+        console.log('index:' + index);
+        console.log('moj index:' + indx);
+        // console.log(index % colums);
       })
     })
     titleTable.push(title);
-    this.index++;
+    this.indx++;
 
     this.setState({
       list: lista,
@@ -69,15 +102,28 @@ class App extends Component {
 
   render() {
 
+    const codeItem = this.state.list.map(item => {
+      // this.counter++;
+      return (<Item key={item.item} item={item.item} title={this.state.titleTable[item.indx]} />)
+    })
+
+    const titleItem = this.state.titleTable.map(item => (
+      <Title key={item.item} title={item.item} />
+    ))
+
+
     return (
       <React.Fragment>
         {!this.state.off ? <Inputer {...this.state} changeTextFn={this.handleChangeText} hideFn={this.handleButtonHide} addFn={this.handleButtonAdd} titleFn={this.handleChangeTitle} /> : null}
-        <div className="codeTable">
-          {this.state.list.map(item => {
-            this.counter++;
-            return (<Item key={item.item} item={item.item} title={this.state.titleTable[item.index]} index={this.counter} />)
-          })
-          }
+        <div className='wrapper'>
+          {/* <div className='test'>Test</div> */}
+          {codeItem}
+          {/* <div className='itemName'>
+            {titleItem}
+          </div> */}
+          {/* <div className="codeTable">
+            {codeItem}
+          </div> */}
         </div>
       </React.Fragment >
     );
--- src/Inputer.js
+++ src/Inputer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 
-const Inputer = ({ changeTextFn, hideFn, addFn, titleFn, ...props }) => (
+const Inputer = ({ changeTextFn, hideFn, addFn, titleFn, text, title, ...props }) => (
   <div disabled={props.off ? true : false} className="App">
-    <input placeholder='Wpisz nazwę sprzetu' onChange={titleFn} value={props.title} />
-    <input placeholder='Wklej numery seryjne' type="text" onChange={changeTextFn} value={props.text} />
+    <input placeholder='Wpisz nazwę sprzetu' onChange={titleFn} value={title} />
+    <input placeholder='Wklej numery seryjne' type="text" onChange={changeTextFn} value={text} />
     <button onClick={addFn}>Dodaj</button>
     <button onClick={hideFn}>Ukryj</button>
   </div>

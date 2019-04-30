07b7769fb7f8e4f2cd240210d6e990afe1570a7a
Michał Sypniewski <mi.sypniewski@gmail.com>
2019-04-30 14:29:42 +0200

    changes of array

--- src/App.js
+++ src/App.js
@@ -7,18 +7,14 @@ const Item = (props) => (
   // <p>{props.index % 2 ? <span>{props.title}</span> : null}<span className="code">{`*${props.item.toUpperCase()}*`}</span></p>
 
   <div className='code'>
-    {`*${props.item.toUpperCase()}*`}
+    {`*${props.title}*`}
     {/* <span>{props.title}</span> */}
     {/* <span className="code">{`*${props.item.toUpperCase()}*`}</span> */}
   </div>
 )
 
 
-const Title = (props) => (
-  <p>
-    <span>{props.title}</span>
-  </p>
-)
+
 
 class App extends Component {
 
@@ -30,51 +26,24 @@ class App extends Component {
     list: [],
     off: false,
     title: '',
-    titleTable: [],
-    // colums: 2,
   })
 
-  addItem = (e, newItem) => {
-    e.preventDefault();
-
-    this.setState(prevState => ({
-      [newItem.type]: [...prevState[newItem.type], newItem]
-    }))
-  }
 
   handleButtonAdd = () => {
-    const lista = this.state.list;
     const text = this.state.text;
     const title = this.state.title;
-    const titleTable = this.state.titleTable;
-    const indx = this.indx;
-    // const colums = this.state.colums;
     const split = text.split(' ');
-    console.log(split)
-    split.forEach((item, index) => {
-      lista.push({
-        item: title,
-      })
-      const tmp = item.split('\t');
-      console.log(tmp);
-      //to jest kluczem do sukcesu
-      tmp.forEach((item, index2) => {
-        console.log('pojedyncze cos ' + item);
-        // if (index % { colums }) {
-        lista.push({
-          item,
-        });
-        // }
-        console.log('index:' + index);
-        console.log('moj index:' + indx);
-        // console.log(index % colums);
-      })
+    split.forEach(items => {
+      const tmp = items.split('\t');
+      const item = {
+        title: title,
+        serials: tmp,
+      }
+      this.setState(prevState => ({
+        list: [...prevState.list, item]
+      }))
     })
-    titleTable.push(title);
-    this.indx++;
-
     this.setState({
-      list: lista,
       text: '',
       title: '',
     })
@@ -102,14 +71,15 @@ class App extends Component {
 
   render() {
 
-    const codeItem = this.state.list.map(item => {
-      // this.counter++;
-      return (<Item key={item.item} item={item.item} title={this.state.titleTable[item.indx]} />)
-    })
+    // const codeItem = this.state.list.map(item => {
+    //   // this.counter++;
+    //   return (<Item key={item.item} item={item.item} serials={item.item} />)
+    //   // return (<Item key={item.item} item={item.item} title={this.state.titleTable[item.indx]} />)
+    // })
 
-    const titleItem = this.state.titleTable.map(item => (
-      <Title key={item.item} title={item.item} />
-    ))
+    // const titleItem = this.state.titleTable.map(item => (
+    //   <Title key={item.item} title={item.item} />
+    // ))
 
 
     return (
@@ -117,7 +87,8 @@ class App extends Component {
         {!this.state.off ? <Inputer {...this.state} changeTextFn={this.handleChangeText} hideFn={this.handleButtonHide} addFn={this.handleButtonAdd} titleFn={this.handleChangeTitle} /> : null}
         <div className='wrapper'>
           {/* <div className='test'>Test</div> */}
-          {codeItem}
+          {/* {codeItem} */}
+          {/* <Item lista={this.state.list} /> */}
           {/* <div className='itemName'>
             {titleItem}
           </div> */}

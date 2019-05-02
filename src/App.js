e5976d7a89207008903e84a6bd8c4b8d62f1c4c7
Michał Sypniewski <mi.sypniewski@gmail.com>
2019-05-02 12:14:09 +0200

    add colums

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Inputer from './Inputer';
+import styled from 'styled-components';
 
+
+
+const Containter = styled('div')`
+    display: grid;
+    grid-template-columns: repeat(${(props) => props.number}, 1fr);
+    grid-template-rows: repeat(auto - fill, 1fr);
+`
 
 const Name = (props) => (
   <div className='name'>{props.title}</div>
 )
 
 const Serial = (props) => (
-  <div className='code'>{props.serial}</div>
+  <div className='code'>{`*${props.serial}*`}</div>
 )
 
 const Items = (props) => {
@@ -31,20 +39,52 @@ const Items = (props) => {
 
 class App extends Component {
 
-  counter = 0;
-  indx = 0;
-
   state = ({
     text: '',
     list: [],
     off: false,
     title: '',
+    columns: 3,
   })
 
 
   handleButtonAdd = () => {
     const text = this.state.text;
     const title = this.state.title;
+
+    const exceptions = 'CCH ';
+
+
+    // let count = 0;
+    // let position = text.indexOf(exceptions);
+    // // let splitt= '';
+
+    // while (position !== -1) {
+    //   count++;
+    //   position = text.indexOf(exceptions, position + 1);
+    //   let splitt = text.split(' ');
+    //   splitt.forEach(items => {
+    //   const tmp = items.split('\t');
+    //   const item = {
+    //     title: title,
+    //     serials: tmp,
+    //   }
+    //   this.setState(prevState => ({
+    //     list: [...prevState.list, item]
+    //   }))
+    // })
+    // this.setState({
+    //   text: '',
+    //   title: '',
+    // })
+    // }
+
+    // console.log(count); // displays 4
+
+
+
+    // console.log(text);
+    // console.log(text.indexOf(exceptions));
     const split = text.split(' ');
     split.forEach(items => {
       const tmp = items.split('\t');
@@ -75,6 +115,12 @@ class App extends Component {
     })
   }
 
+  handleChangeColums = (e) => {
+    this.setState({
+      columns: e.target.value,
+    })
+  }
+
   handleChangeTitle = (e) => {
     this.setState({
       title: e.target.value,
@@ -83,12 +129,22 @@ class App extends Component {
 
   //dodać liniowo CSS albo styled components zeby zrobić na kilka kolumn!!!!
   render() {
+
+    // const number = this.state.columns;
+
     return (
       <React.Fragment>
-        {!this.state.off ? <Inputer {...this.state} changeTextFn={this.handleChangeText} hideFn={this.handleButtonHide} addFn={this.handleButtonAdd} titleFn={this.handleChangeTitle} /> : null}
-        <div className='wrapper'>
+        {!this.state.off ? <Inputer
+          {...this.state}
+          changeTextFn={this.handleChangeText}
+          hideFn={this.handleButtonHide}
+          addFn={this.handleButtonAdd}
+          titleFn={this.handleChangeTitle}
+          columsFn={this.handleChangeColums}
+        /> : null}
+        <Containter number={this.state.columns}>
           {this.state.list.length > 0 && <Items list={this.state.list} />}
-        </div>
+        </Containter>
       </React.Fragment >
     );
   }
--- src/Inputer.js
+++ src/Inputer.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const NumberInputer = styled.input`
+  width: 40px;
+  margin-left: 10px;
+`
 
-const Inputer = ({ changeTextFn, hideFn, addFn, titleFn, text, title, ...props }) => (
+const Inputer = ({ changeTextFn, hideFn, addFn, titleFn, columsFn, text, title, columns, ...props }) => (
   <div disabled={props.off ? true : false} className="App">
     <input placeholder='Wpisz nazwę sprzetu' onChange={titleFn} value={title} />
     <input placeholder='Wklej numery seryjne' type="text" onChange={changeTextFn} value={text} />
     <button onClick={addFn}>Dodaj</button>
     <button onClick={hideFn}>Ukryj</button>
+    <label>
+      Kolumny:
+      <NumberInputer type='number' min='2' max='9' onChange={columsFn} value={columns} />
+    </label>
   </div>
 )
 
